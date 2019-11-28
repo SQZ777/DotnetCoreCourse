534e52095a64fe7ae287bd3a894e95006fb5b418
noctishsu <noctishsu@91app.com>
2019-11-28 16:57:38 +0800

    feat: add initial shell file

--- Course.Tools/dev.sh
+++
@@ -1,4 +0,0 @@
-# install mysql db
-docker run --name dev-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql
-# install postgres db
-docker run --name dev-postgres -p 5432:5432 -e POSTGRES_PASSWORD=password -d postgres
